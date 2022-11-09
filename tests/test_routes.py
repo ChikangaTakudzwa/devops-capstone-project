2f3673c4bb67fe18ec3a0456428ef8215a604eb2
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-11-09 05:36:48 -0500

    Done update feature

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -142,25 +142,21 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_account(self):
-        """It should Update an existing Account"""
+        """Update an existing Account"""
         # create an Account to update
-
         test_account = AccountFactory()
-        # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
-        resp = self.client.post(f"{BASE_URL}", json=test_account.serialize())
-        # assert that the resp.status_code is status.HTTP_201_CREATED
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
         # update the account
-
-        # get the data from resp.get_json() as new_account
         new_account = resp.get_json()
-        # change new_account["name"] to something known
-        new_account.name = ["Takudzwa"]
-        # send a self.client.put() request to the BASE_URL with a json payload of new_account
-        response = self.client.put(f"{BASE_URL}", json=new_account)
-        # assert that the resp.status_code is status.HTTP_200_OK
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # get the data from resp.get_json() as updated_account
-        updated_acc = response.get_json()
-        # assert that the updated_account["name"] is whatever you changed it to
-        self.assertEqual(updated_acc.name, "Takudzwa")
+        new_account["name"] = "Takudzwa Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Takudzwa Known")
+
+    # def test_get_account_not_found(self):
+    #     """Not Read an Account that is not found"""
+    #     account = self._create_accounts(1)[0]
+    #     resp = self.client.put(f"{BASE_URL}/{account[""]}")
+    #     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
