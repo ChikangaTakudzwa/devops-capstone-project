e52d1ce15c55e7f2f7ab8696a1572cbb88b6514e
ChikangaTakudzwa <youngpainzw@gmail.com>
2022-11-10 05:18:40 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -72,8 +72,8 @@ def list_accounts():
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list)
-
     return make_response(jsonify(account_list), status.HTTP_200_OK)
+    
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
